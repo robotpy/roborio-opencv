bfb38eba6d35618bc5113fb89bbec5b58ef1c29a
Dustin Spicuzza <dustin@virtualroadside.com>
2023-03-14 11:13:34 -0400

    Bump other version too

--- data-py.py
+++ data-py.py
@@ -1,5 +1,5 @@
 name = "robotpy-opencv"
-version = "4.6.0+r1"
+version = "4.6.0+r2"
 url = "https://opencv.org"
 license = "Apache 2"
 install_requires = ["numpy", f"robotpy-opencv-core=={version}"]
